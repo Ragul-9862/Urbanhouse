b0d88306711336f66bf9672ab5aaa0b06b29ae45
Ragul-9862 <Ragulragul9862@gmail.com>
2023-11-29 21:36:46 +0530

    final commit

--- src/Components/Pages/Home.js
+++ src/Components/Pages/Home.js
@@ -4,7 +4,7 @@ import Companies from '../Companies'
 import Guide from '../Guide'
 import Properties from '../Properties'
 import Details from '../Details'
-
+import GetStarted from '../Getstarted'
 import Footer from '../Footer'
 
 export default function Home() {
@@ -15,6 +15,7 @@ export default function Home() {
         <Guide/>
         <Properties/>
         <Details/>
+        <GetStarted/>
         <Footer/>
     </div>
   )
